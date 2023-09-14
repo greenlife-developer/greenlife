a17f5439b266b5feac00f8fa5ef19bb5116e2001
Afolabi Opeyemi <yemijoshua81@gmail.com>
2023-09-14 06:27:06 +0100

    adding a new testimonial

--- client/src/components/home/ClientsTestimony.jsx
+++ client/src/components/home/ClientsTestimony.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import testimony1 from "../../images/testimony/testimony-1.jpg";
+import testimony2 from "../../images/testimony/testimony-2.jpg";
 import TestimonialItem from "./TestimonialItem";
 import team3 from "./TestimonialItem";
 
@@ -21,10 +22,10 @@ export default function ClientsTestimony() {
             content="We train students on HTML, CSS (grid and flexbox), Javascript, git and github, and how to host a static website. We train students on HTML, CSS (grid and flexbox), Javascript, git and github, and how to host a static website."
           />
           <TestimonialItem
-            img={team3}
-            name="Afolabi Opeyemi"
-            title="CEO, GNLife"
-            content="We train students on HTML, CSS (grid and flexbox), Javascript, git and github, and how to host a static website. We train students on HTML, CSS (grid and flexbox), Javascript, git and github, and how to host a static website."
+            img={testimony2}
+            name="Boyede Osakuade"
+            title="CEO, EYOB Agrochemicals"
+            content="GNLIFE helped in developing the app that I use for taking the records of my daily sales and purchases. The app is so useful that I can easily access what I have in store both in figure and cash value."
           />
           <TestimonialItem
             img={team3}
